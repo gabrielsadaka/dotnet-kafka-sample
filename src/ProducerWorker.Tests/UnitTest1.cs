c5d80a8feb70e55ca525e0f92d21d0a67a1d8d6a
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-07-16 14:27:57 +1000

    Add test project

---
+++ src/ProducerWorker.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ProducerWorker.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
