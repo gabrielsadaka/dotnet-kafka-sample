04c6236812c50fffc0a58c005153ea6b56d43922
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-07-19 16:55:26 +1000

    Add Dockerfile for consumer

--- src/ConsumerWorker/Program.cs
+++ src/ConsumerWorker/Program.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,11 +10,10 @@ namespace ConsumerWorker
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) => { services.AddHostedService<Worker>(); });
+        }
     }
 }
